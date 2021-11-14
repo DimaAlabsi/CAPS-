0ad65a34e2e85f5375cab226d4e73c9a4be157fd
DimaAlabsi <dema.yaser8@gmail.com>
2021-11-14 17:25:31 +0200

    lab11

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # CAPS-
+
+
+### Event Driven Applications
+
+## Author: Dima Alabsi
+
+
+
+## Running the app
+
+* node caps.js
+* nodemon
+
+
+
+
+## Tests
+
+* Unit Tests: **npm run test**
+
+
+
+
+
+## Github actions
+
+*    [actions](https://github.com/DimaAlabsi/CAPS-/actions)      
+
+
+## pull request
+
+
+* [last pull request](https://github.com/DimaAlabsi/CAPS-/pull/1)
+
+
+# dependencies 💯
+
+
+cors
+dotenv
+express
+faker
+jest
+supertest
+
+    
+
+## UML
+
+
+
+
+
+
+
+
+![uml](img/uml.png)
+
+![test](img/testingResults.png)
+
+![console](img/consoleResults.png)
---
+++ caps.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const events = require('./events');
+require('./driver/driver');
+require('./vendor/vendor');
+
+
+
+const pickup = (payload) => {
+    let consoleOutput = {
+        event: 'pickup',
+        time: new Date(),
+        payload: payload
+    }
+    console.log('Event', consoleOutput);
+};
+
+events.on('pickup', pickup);
+
+const intransit = (payload) => {
+    let consoleOutput = {
+        event: 'in-transit',
+        time: new Date(),
+
+        payload: payload
+    };
+    console.log('Event', consoleOutput);
+};
+events.on('in-transit', intransit);
+
+const delivered = (payload) => {
+    const consoleOutput = {
+        event: 'delivered',
+        time: new Date(),
+        payload: payload
+    }
+    console.log('Event', consoleOutput);
+}
+events.on('delivered', delivered);
+
+
+module.exports = {
+    pickup,
+    intransit,
+    delivered
+}
+
+// EVENT { event: 'pickup',
+//   time: 2020-03-06T18:27:17.732Z,
+//   payload:
+//    { store: '1-206-flowers',
+//      orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
+//      customer: 'Jamal Braun',
+//      address: 'Schmittfort, LA' } }
+// DRIVER: picked up e3669048-7313-427b-b6cc-74010ca1f8f0
+// EVENT { event: 'in-transit',
+//   time: 2020-03-06T18:27:18.738Z,
+//   payload:
+//    { store: '1-206-flowers',
+//      orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
+//      customer: 'Jamal Braun',
+//      address: 'Schmittfort, LA' } }
+// DRIVER: delivered up e3669048-7313-427b-b6cc-74010ca1f8f0
+// VENDOR: Thank you for delivering e3669048-7313-427b-b6cc-74010ca1f8f0
+// EVENT { event: 'delivered',
+//   time: 2020-03-06T18:27:20.736Z,
+//   payload:
+//    { store: '1-206-flowers',
+//      orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
+//      customer: 'Jamal Braun',
+//      address: 'Schmittfort, LA' } }
